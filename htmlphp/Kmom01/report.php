4853b7f7f6a1c1880d874df8868142219bb3975f
IcePhenom <madshoffnielsen@gmail.com>
2015-04-11 16:46:39 +0200

    htmlphp-kmom01-initial

---
+++ htmlphp/Kmom01/report.php
@@ -0,0 +1,18 @@
+<?php include("incl/config.php"); ?>
+
+<?php $title = "Mina redovisningar av kursmomenten"; ?>
+
+<?php include("incl/header.php"); ?>
+
+<!-- Sidans/Dokumentets huvudsakliga innehåll -->
+<h1>Redovisning av kursmomenten</h1>
+<h2>Kmom01: Kom igång med HTML, CSS och PHP</h2>
+<p>Momentet var väldigt enkelt då jag tidigare har syslat med HTML och webbutveckling.<br>
+Men en bra och enkel grund för de som ej tidigare har hållt på med HTML.<br>
+Jag stötte inte på några problem då jag även har använt BTH system för webbsidor i tidigare kurser.</p>
+<p>Valde även att modifiera CSS till att använda lite andra färger på sidan än standard.</p>
+<p>Sitter på flera olika plattformar när jag utvecklar, Windows 7 Pro 64-bit, Linux Mint 13 (Gammal laptop) samt lite Mac OSX.<br>
+Gemensamt är dock verktygen jag använder, <strong>Sublime Text 3</strong> med plugins för webbutveckling, <strong>XAMPP</strong> (Apache + MySQL + PHP) för att köra sidan lokalt när jag utvecklar samt <strong>Filezilla</strong> för FTP.</p>
+<p>För att visa sidan använder jag både Firefox och Chrome beroende på utvecklingsplatform.</p>
+
+<?php include("incl/footer.php"); ?>
