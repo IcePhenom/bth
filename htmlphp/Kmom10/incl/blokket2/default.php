3d7748964215d085425f55f7c8053fe105986f9f
IcePhenom <madshoffnielsen@gmail.com>
2015-04-11 16:48:47 +0200

    htmlphp-kmom10-initial

---
+++ htmlphp/Kmom10/incl/blokket2/default.php
@@ -0,0 +1,6 @@
+ <!-- Sidans/Dokumentets huvudsakliga innehåll -->
+<h1>Blokket2, annonsplatsen nu med SQLite</h1>
+
+<p>Lägg upp dina annonser och hoppas att någon ser dem och köper...
+</p>
+<p>Välj funktioner från menyn till vänster.</p>
---
+++ htmlphp/Kmom10/incl/header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="sv">
+<head>
+	<meta charset="utf-8">
+	<title><?=$title?></title>
+	<!-- links to external stylesheets -->
+	<?php if(isset($_SESSION['stylesheet'])): ?>
+		<link rel="stylesheet" href="style/<?php echo $_SESSION['stylesheet']; ?>">
+	<?php else: ?>
+		<link rel="stylesheet" href="style/stylesheet.css" title="General stylesheet">
+		<link rel="alternate stylesheet" href="style/debug.css" title="Debug stylesheet">
+	<?php endif; ?>
+	<link rel="shortcut icon" href="img/favicon.ico">
+
+	<!-- Each page can set $pageStyle to create an internal stylesheet -->
+	<?php if(isset($pageStyle)) {
+		echo '<style type="text/css">' . $pageStyle . '</style>';
+	} ?>
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<!-- The body id helps with highlighting current menu choice -->
+<body<?php echo isset($pageId)?" id='$pageId' ":""; ?>>
+	<!-- Above header -->
+	<header id="above">
+		<?php echo userLoginMenu(); ?>
+	  <!-- Relateted links -->
+	  <nav class="related">
+	    <a href="../Kmom01/me.php">kmom01</a>
+	    <a href="../Kmom02/me.php">kmom02</a>
+	    <a href="../Kmom03/me.php">kmom03</a>
+	    <a href="../Kmom04/me.php">kmom04</a>
+	  </nav>
+	</header>
+	<!-- Header -->
+	<header>
+		<?php if(isset($_SESSION['stylesheet']) && $_SESSION['stylesheet'] == "fancy.css"): ?>
+		<a href="me.php"><img src="img/fancy.png" alt="fancy logo" width=200 height=90></a>
+		<?php else: ?>
+	    <a href="me.php"><img src="img/logo.png" alt="htmlphp logo" width=300 height=70></a>
+	    <?php endif; ?>
+		<nav class="navmenu">
+		  <a id="bmo-"	href="index.php">BMO</a>
+		  <a id="object-"	href="object.php">Objekt</a>
+		  <a id="article-"	href="article.php">Artiklar</a>
+		  <a id="about-"	href="about.php">Om</a>
+
+		  <a id="me-"     href="me.php">Me</a>
+			<!-- <a id="report-" href="report.php">Redovisning</a>
+			<a id="test-"	href="test.php">Test</a>
+			<a id="style-"	href="style.php">Style</a>
+			<a id="blokket-"	href="blokket.php">Blokket</a>
+			<a id="blokket2-"	href="blokket2.php">Blokket2</a>
+			<a id="source-" href="viewsource.php">Källkod</a> -->
+		</nav>
+	</header>
