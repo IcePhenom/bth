f230bdbcf7ad356f08ae1b4b6f428f5e534289c5
IcePhenom <madshoffnielsen@gmail.com>
2015-04-12 15:37:45 +0200

    Update function.php with new functions

--- htmlphp/Kmom06/incl/function.php
+++ htmlphp/Kmom06/incl/function.php
@@ -43,3 +43,27 @@ function menu() {
     <a id='source-' href='viewsource.php'>Källkod</a><br>
   </nav>";
 }
+
+function tools() {
+  return "<a href='http://validator.w3.org/check/referer'>HTML5</a><br>
+  <a href='http://jigsaw.w3.org/css-validator/check/referer'>CSS</a><br>
+  <a href='http://jigsaw.w3.org/css-validator/check/referer?profile=css3'>CSS3</a><br>
+  <a href='http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance'>Unicorn</a><br>
+  <a href='http://validator.w3.org/i18n-checker/check?uri=<?php echo getCurrentUrl(); ?>'>i18n</a><br>
+  <a href='http://validator.w3.org/checklink?uri=<?php echo getCurrentUrl(); ?>'>Links</a>";
+}
+
+
+function manual() {
+  return "<a href='http://www.w3.org/2009/cheatsheet/'>Cheatsheet</a><br>
+  <a href='http://dev.w3.org/html5/spec/'>HTML5</a><br>
+  <a href='http://www.w3.org/TR/CSS2/'>CSS2</a><br>
+  <a href='http://www.w3.org/Style/CSS/current-work#CSS3'>CSS3</a><br>
+  <a href='http://php.net/manual/en/index.php'>PHP</a>";
+}
+
+function pageTime($pageTimeGeneration) {
+  if(isset($pageTimeGeneration)) {
+    return "Page generated in " . round(microtime(true)-$pageTimeGeneration, 5) . " seconds";
+  }
+}
