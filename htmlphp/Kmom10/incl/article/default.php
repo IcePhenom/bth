579ea0c7c460aaa5d5099770f2946f7fea908f2e
IcePhenom <madshoffnielsen@gmail.com>
2015-05-17 11:59:46 +0200

    Add javascript selector to articles

--- htmlphp/Kmom10/incl/article/default.php
+++ htmlphp/Kmom10/incl/article/default.php
@@ -11,11 +11,54 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // Display errors, b
 $stmt = $db->prepare('SELECT * FROM Article WHERE category = "article" ORDER BY pubdate DESC');
 $stmt->execute();
 $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
 
-<h1>Visa alla artiklar</h1>
-<?php foreach($res as $article): ?>
-  <div class='article'>
+$count = $db->query('SELECT COUNT(*) FROM Article WHERE category = "article"')->fetchColumn();
+?>
+<script type="text/javascript">
+  function changeContent(sel) {
+    var count = "<?php echo $count; ?>";
+    var i;
+    if (sel.value == 'all') {
+      document.getElementById('head-all').style.display = "";
+      document.getElementById('head').style.display = "none";
+      for (i = 0; i < count; i++) {
+        document.getElementById(i).style.display = "";
+      }
+    }
+    else {
+      document.getElementById('head-all').style.display = "none";
+      document.getElementById('head').style.display = "";
+      for (i = 0; i < count; i++) {
+        if (i == sel.value) {
+          document.getElementById(i).style.display = "";
+        }
+        else {
+          document.getElementById(i).style.display = "none";
+        }
+      }
+    }
+  }
+</script>
+
+<div id='selector'>
+  Välj artikel:
+  <select onchange="changeContent(this)">
+    <option value='all'>--Alla--</option>
+    <?php
+    $i = 0;
+    foreach ($res as $art) {
+      echo "<option value='" . $i++ . "'>" . $art['title'] . "</option>";
+    }
+    ?>
+  </select>
+</div>
+
+<div id='head-all'><h1>Alla artiklar</h1></div>
+<div id='head' style='display:none'><h1>Artikel</h1></div>
+<?php
+$i = 0;
+foreach($res as $article): ?>
+  <div class='article' id='<?php echo $i++; ?>'>
     <strong><?php echo $article['title']; ?></strong><br>
     <?php echo $article['content']; ?>
     <p class='articleauth'><?php echo $article['author']; ?></p>
