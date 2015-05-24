cf6def715b6b1b3d29249efcd97691236fdb27f4
IcePhenom <madshoffnielsen@gmail.com>
2015-05-24 16:16:19 +0200

    Editability for front page

--- htmlphp/Kmom10/index.php
+++ htmlphp/Kmom10/index.php
@@ -3,24 +3,27 @@ include("incl/config.php");
 $title = "Begravningsmuseum Online";
 $pageId = "index";
 $pageStyle = '';
+
+$path = "incl/front";
+$file = "default.php";
+f_front($title, $file);
 ?>
 
 <?php include("incl/header.php"); ?>
 
 <!--- Main -->
 <section id="content">
-  <h2>Välkommen till begravningsmuseum online (BMO)</h2>
-  <figure class="about-images">
-    <img class="front-image" src="img/bmo/begravningstal_eric_crispin_segercronas.jpg" alt="Bild på Ronny Holm">
-    <figcaption>
-      <p>Begravningstal<br>Eric Crispin Segercronas</p>
-    </figcaption>
-  </figure>
-  <p>I början av 1970-talet började Ronny Holm, att samla de första sakerna, som nu finns i Begravningsmuseet i Ljungby. Det var ting som funnits i kyrkor, på kyrkogården, från privatpersoner m fl. Under årens lopp har Ronny Holm handlat i second handbutiker, varit på loppmarknader och besökt auktioner.</p>
-
-  <p>I samband med en kyrkogårdsvandring arrangerades en utställning med föremål från Ronny Holms samling. Flera av deltagarna vid rundvandringen tyckte att utställningen var så värdefull att den borde permanentas. I samband med Alla Helgons Dag, 2004, invigdes så Sveriges första begravningsmuseum.</p>
-
-  <p>Redan efter ett par veckor hade man haft över 2000 besökare och intresset var mycket stort från både allmänhet, grupper av alla de slag och inte minst massmedia. Museet visar seder och bruk vid död och begravning under 1800 och 1900-talen. Här visas hela förloppet vid ett dödsfall, från tillkännagivandet via klockringning till gravsättningen och gravölet. Många av föremålen är skänkta eller utlånade av privatpersoner, institutioner etc.</p>
+  <?php if (userIsAuthenticated()) {?>
+    <aside class="right" style="width:22%;">
+      <?php include("$path/aside.php"); ?>
+    </aside>
+    <article class="left border justify-para" style="width:72%;">
+  <?php }
+  else { ?>
+    <article class="left justify-para">
+  <?php } ?>
+    <?php include("$path/$file"); ?>
+    </article>
 </section>
 
 <?php include("incl/footer.php"); ?>
