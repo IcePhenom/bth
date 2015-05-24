5c3c0b1676e617bbace018c272a75ace1728bf27
IcePhenom <madshoffnielsen@gmail.com>
2015-05-24 13:39:22 +0200

    Split gallery functionality into class and style

---
+++ htmlphp/Kmom10/incl/gallery/gallery_main.php
@@ -0,0 +1,30 @@
+<?php
+
+class gallery {
+  private $path;
+  private $perPage;
+  private $images;
+
+  function __construct($path, $perPage) {
+    $this->path = $path;
+    $this->perPage = $perPage;
+  }
+
+  function loadGallery() {
+    $this->images = array();
+
+    foreach (scandir($this->path) as $file) {
+      if ($file != '.' && $file != '..') {
+        $this->images[] = $file;
+      }
+    }
+  }
+
+  function paging() {
+    return count($this->images)/$this->perPage;
+  }
+
+  function listImages($page) {
+    return array_slice($this->images, $page*$this->perPage, $this->perPage);
+  }
+}
---
+++ htmlphp/Kmom10/incl/gallery/gallery_pres.php
@@ -0,0 +1,43 @@
+<?php
+require("gallery_main.php");
+
+?>
+<script type="text/javascript" src="incl/gallery/js/jquery-1.10.1.min.js"></script>
+<script type="text/javascript" src="incl/gallery/js/jquery.fancybox.js?v=2.1.5"></script>
+<link rel="stylesheet" type="text/css" href="incl/gallery/js/jquery.fancybox.css?v=2.1.5" media="screen" />
+<script type="text/javascript">
+  $(document).ready(function() {
+    $(".fancybox").fancybox({
+      helpers : {
+        title : {
+          type : 'inside'
+        },
+        overlay : {
+          css : {
+            'background' : 'rgba(0,0,0,0.75)'
+          }
+        }
+      }
+    });
+  });
+</script>
+<?php
+$p = (isset($_GET['p'])) ? $_GET['p'] : 0;
+
+$gallery = new gallery(dirname(dirname(dirname(__FILE__))) . "/img/bmo/550", 8);
+$gallery->loadGallery();
+
+$pages = $gallery->paging();
+$images = $gallery->listImages($p);
+
+echo '<div class="paging">';
+for ($i=0; $i < $pages; $i++) {
+  echo ($p == $i) ? "Sida ".($i+1)." " : "<a href='gallery.php?p=$i'>Sida ".($i+1)."</a> ";
+}
+echo '</div>';
+
+echo "<ul id='pictures'>";
+foreach ($images as $img) {
+  echo "<li><a class='fancybox' href='img/bmo/$img' data-fancybox-group='a'><img src='img/bmo/250/$img' alt='' /></a></li>";
+}
+echo '</ul>';
